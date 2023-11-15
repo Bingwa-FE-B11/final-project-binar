3448a57b2529591690851d5c86cd4b909ec0f6b8
Ferry Febriansyah <perriebenhard@gmail.com>
2023-11-15 21:45:02 +0700

    initial commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const HomePage = () => {
+  return (
+    <div className='flex justify-center items-center min-h-screen'>
+        <span className='text-5xl font-semibold'>Homepage</span>
+    </div>
+  )
+}
