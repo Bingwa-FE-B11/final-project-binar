0272d7111caf4db72ec5f0b48fe83ca63a8830dd
Ferry Febriansyah <perriebenhard@gmail.com>
2023-11-25 22:06:29 +0700

    feat: make Error404 Page

--- src/routes/RouteList.js
+++ src/routes/RouteList.js
@@ -7,22 +7,27 @@ import { ResetPassword } from '../pages/ResetPassword';
 import { AdminLogin } from '../pages/AdminLogin';
 import { KelasSaya } from '../pages/KelasSaya';
 import { Otp } from '../pages/Otp';
-import { BerandaPage } from '../pages/BerandaPage';
+import { Error404 } from '../pages/errors/Error404';
 
 export const RouteList = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/homepage" element={<HomePage />} />
+
+        {/* User */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<Register />} />
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/otp" element={<Otp />} />
-        <Route path="/beranda" element={<BerandaPage />} />
         <Route path="/kelas-saya" element={<KelasSaya />} />
 
         {/* Admin */}
         <Route path="/admin/login" element={<AdminLogin />} />
+
+        {/* Error */}
+        <Route path="*" element={<Error404 />} />
       </Routes>
     </BrowserRouter>
   );
