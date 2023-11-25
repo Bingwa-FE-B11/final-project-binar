f69d961eeb5c5ab1a360d001ee777b86d56e063c
Bagus Dwi Putra Adiyono <134615628+Bagusdpa4@users.noreply.github.com>
2023-11-25 13:04:05 +0700

    #4 Make Kelas Saya Page
    Co-authored-by: Ferry Febriansyah <96556755+perriee@users.noreply.github.com>

--- src/routes/RouteList.js
+++ src/routes/RouteList.js
@@ -4,6 +4,7 @@ import { HomePage } from '../pages/HomePage'
 import { LoginPage } from '../pages/LoginPage'
 import { Register } from '../pages/Register'
 import { ResetPassword } from '../pages/ResetPassword'
+import { KelasSaya } from '../pages/KelasSaya'
 import { AdminLogin } from '../pages/AdminLogin'
 import { Otp } from '../pages/Otp'
 
@@ -15,6 +16,7 @@ export const RouteList = () => {
             <Route path='/Login' element={<LoginPage/>}/>
             <Route path='/Register' element={<Register/>}/>
             <Route path='/ResetPassword' element={<ResetPassword/>}/>
+            <Route path='/KelasSaya' element={<KelasSaya/>}/>
             <Route path='/AdminLogin' element={<AdminLogin/>}/>
             <Route path='/OTP' element={<Otp/>}/>
         </Routes>
