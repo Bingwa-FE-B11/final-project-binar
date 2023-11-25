ccc4e4f7fea3ee6a0c644b97100d8c3f8668c840
Ferry Febriansyah <perriebenhard@gmail.com>
2023-11-25 13:51:19 +0700

    feat: make Beranda Page

--- src/routes/RouteList.js
+++ src/routes/RouteList.js
@@ -1,23 +1,25 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { HomePage } from '../pages/HomePage'
-import { LoginPage } from '../pages/LoginPage'
-import { Register } from '../pages/Register'
-import { ResetPassword } from '../pages/ResetPassword'
-import { AdminLogin } from '../pages/AdminLogin'
-import { Otp } from '../pages/Otp'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HomePage } from '../pages/HomePage';
+import { LoginPage } from '../pages/LoginPage';
+import { Register } from '../pages/Register';
+import { ResetPassword } from '../pages/ResetPassword';
+import { AdminLogin } from '../pages/AdminLogin';
+import { Otp } from '../pages/Otp';
+import { BerandaPage } from '../pages/BerandaPage';
 
 export const RouteList = () => {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path='/Login' element={<LoginPage/>}/>
-            <Route path='/Register' element={<Register/>}/>
-            <Route path='/ResetPassword' element={<ResetPassword/>}/>
-            <Route path='/AdminLogin' element={<AdminLogin/>}/>
-            <Route path='/OTP' element={<Otp/>}/>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Login" element={<LoginPage />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/ResetPassword" element={<ResetPassword />} />
+        <Route path="/AdminLogin" element={<AdminLogin />} />
+        <Route path="/OTP" element={<Otp />} />
+        <Route path="/beranda" element={<BerandaPage />} />
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
