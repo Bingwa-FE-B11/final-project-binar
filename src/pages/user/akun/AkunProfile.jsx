13d55807068d5c1a5d98cf3802d1aab09efa43b6
Adindawww <140032812+Adindawww@users.noreply.github.com>
2023-12-14 12:55:04 +0700

    #32 Get userProfile in AkunProfile Page

--- src/pages/user/akun/AkunProfile.jsx
+++ src/pages/user/akun/AkunProfile.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 // Components
@@ -8,35 +8,48 @@ import { SidebarAkun } from '../../../assets/components/sidebar/SidebarAkun';
 // Icons
 import { GoArrowLeft } from 'react-icons/go';
 import { IoImageOutline } from 'react-icons/io5';
+import { useSelector } from 'react-redux';
+
 
 export const AkunProfile = () => {
+  const Data = useSelector((state) => state.authLogin);
   const navigate = useNavigate();
+  const [user, setUser] = useState({
+    image: "",
+    fullName: "",
+    phoneNumber: "",
+    country: "",
+    city: "",
+  });
+  console.log("Data", Data)
+
+
   return (
     <>
-      <div className="mt-[5rem] bg-secondary h-[10rem] lg:px-80">
-        <div className="flex items-center py-8 lg:px-0 px-2 gap-2 text-lg font-bold text-primary lg:relative">
+      <div className="mt-[5rem] h-[10rem] bg-secondary lg:px-80">
+        <div className="flex items-center gap-2 px-2 py-8 text-lg font-bold text-primary lg:relative lg:px-0">
           <GoArrowLeft
             size={30}
             className="cursor-pointer lg:absolute lg:-inset-x-16"
             onClick={() => {
-              navigate('/kelas-saya');
+              navigate("/kelas-saya");
             }}
           />
           Kembali Ke Beranda
         </div>
 
         {/* Akun */}
-        <div className="border-2 border-primary rounded-xl">
-          <div className="py-4 text-xl font-semibold text-center text-white rounded-t-lg bg-primary">
+        <div className="rounded-xl border-2 border-primary">
+          <div className="rounded-t-lg bg-primary py-4 text-center text-xl font-semibold text-white">
             Akun
           </div>
 
           {/* Isi Akun */}
           <div className="flex py-4 text-center">
             <SidebarAkun />
-            <div className="flex flex-col items-center w-[60%] gap-4">
-              <div className="w-20 h-20 border-[3px] rounded-full border-primary relative">
-                <div className="absolute bottom-0 right-0 bg-white rounded-full text-primary w-fit">
+            <div className="flex w-[60%] flex-col items-center gap-4">
+              <div className="relative h-20 w-20 rounded-full border-[3px] border-primary">
+                <div className="absolute bottom-0 right-0 w-fit rounded-full bg-white text-primary">
                   <IoImageOutline size={25} />
                 </div>
               </div>
@@ -44,31 +57,34 @@ export const AkunProfile = () => {
                 <div className="text-left">Nama</div>
                 <input
                   type="text"
-                  className="px-4 py-3 border-2 w-[22rem] rounded-2xl border-slate-300 focus:outline-none focus:border-primary"
+                  className="w-[22rem] rounded-2xl border-2 border-slate-300 px-4 py-3 focus:border-primary focus:outline-none"
                   placeholder="Bingwa"
+                  value={Data.user.userProfile.fullName}
                 />
               </div>
               <div className="flex flex-col gap-1">
                 <div className="text-left">Email</div>
                 <input
                   type="text"
-                  className="px-4 py-3 border-2 w-[22rem] rounded-2xl border-slate-300 focus:outline-none focus:border-primary"
+                  className="w-[22rem] rounded-2xl border-2 border-slate-300 px-4 py-3 focus:border-primary focus:outline-none"
                   placeholder="bingwa@gmail.com"
+                  value={Data.user.email}
                 />
               </div>
               <div className="flex flex-col gap-1">
                 <div className="text-left">Nomor Telepon</div>
                 <input
                   type="text"
-                  className="px-4 py-3 border-2 w-[22rem] rounded-2xl border-slate-300 focus:outline-none focus:border-primary"
+                  className="w-[22rem] rounded-2xl border-2 border-slate-300 px-4 py-3 focus:border-primary focus:outline-none"
                   placeholder="08123456789"
+                  value={Data.user.userProfile.phoneNumber}
                 />
               </div>
               <div className="flex flex-col gap-1">
                 <div className="text-left">Negara</div>
                 <input
                   type="text"
-                  className="px-4 py-3 border-2 w-[22rem] rounded-2xl border-slate-300 focus:outline-none focus:border-primary"
+                  className="w-[22rem] rounded-2xl border-2 border-slate-300 px-4 py-3 focus:border-primary focus:outline-none"
                   placeholder="Indonesia"
                 />
               </div>
@@ -76,11 +92,11 @@ export const AkunProfile = () => {
                 <div className="text-left">Kota</div>
                 <input
                   type="text"
-                  className="px-4 py-3 border-2 w-[22rem] rounded-2xl border-slate-300 focus:outline-none focus:border-primary"
+                  className="w-[22rem] rounded-2xl border-2 border-slate-300 px-4 py-3 focus:border-primary focus:outline-none"
                   placeholder="Jakarta"
                 />
               </div>
-              <button className="px-4 py-3 font-semibold text-white w-[22rem] bg-primary rounded-2xl hover:bg-primary-hover">
+              <button className="w-[22rem] rounded-2xl bg-primary px-4 py-3 font-semibold text-white hover:bg-primary-hover">
                 Simpan Profil Saya
               </button>
             </div>
