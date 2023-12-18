53f173184b3c973067554db38a995b4c437a3885
adindawww <adindawahyu680@gmail.com>
2023-12-19 05:53:30 +0700

    upload Getkelasgratis

--- src/services/courses/getAllCourses.js
+++ src/services/courses/getAllCourses.js
@@ -4,5 +4,8 @@ import { API_ENDPOINT } from "../../utils/api-endpoint";
 import http from "../../utils/http";
 
 export const reduxGetAllCourses = async () => {
-  return await http.get(API_ENDPOINT.GET_ALL_COURSES);
+  return await http.get(`${API_ENDPOINT.GET_ALL_COURSES}?limit=50`);
 };
+export const reduxGetDetailCoursesId = async () => {
+  return await http.get(API_ENDPOINT.GET_COURSE);
+}
