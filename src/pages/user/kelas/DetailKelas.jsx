37f45feb9ed14a714d6309fac3252c71302f1ceb
Bagus Dwi <sugab.dwi88@gmail.com>
2023-12-19 16:39:31 +0700

    Integrasi Detail Kelas

--- src/pages/user/kelas/DetailKelas.jsx
+++ src/pages/user/kelas/DetailKelas.jsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { NavbarKelas } from "../../../assets/components/navbar/NavbarKelas";
 import { CardKursus } from "../../../assets/components/cards/CardKursus";
 import { NavbarHome } from "../../../assets/components/navbar/NavbarHome";
+import CardCoursesSkeleton from "../../../assets/components/skeleton/CardCourseSkeleton";
 
 // Icons
 import { GoArrowLeft } from "react-icons/go";
@@ -43,27 +44,27 @@ export const DetailKelas = () => {
   const handleDialogOpen = () => setDialogOpen(!dialogOpen);
 
   const handleClickBack = () => {
-    window.history.back()
+    window.history.back();
   };
 
   useEffect(() => {
     getDetailCourses();
   }, [dispatch]);
 
-  console.log("data detail",storeDetailCourses);
+  console.log("data detail", storeDetailCourses);
 
   return (
     <>
       {storeAuthUser.token === null ? <NavbarHome /> : <NavbarKelas />}
 
       {/* Parent Container */}
-      <div className="z-20 flex min-h-screen px-0 lg:px-20 md:px-4 py-6">
+      <div className="z-20 flex min-h-screen px-0 py-6 md:px-4 lg:px-20">
         {/* Left Container */}
-        <div className="mt-16 flex w-full lg:w-2/3 md:w-2/3 flex-col gap-4 px-8">
+        <div className="mt-16 flex w-full flex-col gap-4 px-8 md:w-2/3 lg:w-2/3">
           {/* Button Back */}
           <div className="flex w-full items-center gap-2 py-4">
             <div className="cursor-pointer" onClick={handleClickBack}>
-              <GoArrowLeft size={30}/>
+              <GoArrowLeft size={30} />
             </div>
             <div className="font-semibold">Kelas Lainnya</div>
           </div>
@@ -71,12 +72,16 @@ export const DetailKelas = () => {
           {/* Container Desc Kelas */}
           <div className="flex flex-col gap-3">
             <div className="flex justify-between">
-              <div className="text-xl font-bold text-primary">{storeDetailCourses.category.categoryName}</div>
+              <div className="text-xl font-bold text-primary">
+                {storeDetailCourses?.category?.categoryName}
+              </div>
               <div className="flex items-center gap-1">
                 <div className="text-yellow-500">
                   <FaStar />
                 </div>
-                <div className="text-lg font-bold">{storeDetailCourses?.averageRating}</div>
+                <div className="text-lg font-bold">
+                  {storeDetailCourses?.averageRating}4.9
+                </div>
               </div>
             </div>
             <div className="flex flex-col">
@@ -85,7 +90,7 @@ export const DetailKelas = () => {
               </div>
               <div className="text-lg">{storeDetailCourses?.mentor}</div>
             </div>
-            <div className="flex gap-4 lg:gap-10 md:gap-10">
+            <div className="flex gap-4 md:gap-10 lg:gap-10">
               <div className="flex items-center gap-1">
                 <RiShieldStarLine size={20} color="#22c55e" />
                 <div className="text-sm font-semibold text-primary">
@@ -94,16 +99,22 @@ export const DetailKelas = () => {
               </div>
               <div className="flex items-center gap-1">
                 <LiaBookSolid size={20} color="#22c55e" />
-                <div className="text-sm font-semibold">{storeDetailCourses?.modul}</div>
+                <div className="text-sm font-semibold">
+                  {storeDetailCourses?.modul}
+                </div>
               </div>
               <div className="flex items-center gap-1">
                 <IoTime size={20} color="#22c55e" />
-                <div className="text-sm font-semibold">{storeDetailCourses?.duration}</div>
+                <div className="text-sm font-semibold">
+                  {storeDetailCourses?.duration}
+                </div>
               </div>
             </div>
           </div>
-          <div className="flex w-fit cursor-pointer items-center gap-2 rounded-xl bg-green px-6 py-2 text-white"
-          onClick={()=> window.open(storeDetailCourses?.forumURL, "_blank")}>
+          <div
+            className="flex w-fit cursor-pointer items-center gap-2 rounded-xl bg-green px-6 py-2 text-white"
+            onClick={() => window.open(storeDetailCourses?.forumURL, "_blank")}
+          >
             <div className="font-semibold">Join Grup Telegram</div>
             <div>
               <HiChatAlt2 size={20} />
@@ -123,7 +134,7 @@ export const DetailKelas = () => {
               <div className="flex flex-col gap-2">
                 <h1 className="text-xl font-bold">Tentang Kelas</h1>
                 <p className="text-slate-600">
-                {storeDetailCourses?.aboutCourse}
+                  {storeDetailCourses?.aboutCourse}
                 </p>
               </div>
 
@@ -139,14 +150,18 @@ export const DetailKelas = () => {
         </div>
 
         {/* Right Container */}
-        <div className="mt-20 w-1/3 flex-col hidden lg:flex md:flex">
+        <div className="mt-20 hidden w-1/3 flex-col md:flex lg:flex">
           {/* Sidebar */}
           <div className="mt-8 flex flex-col gap-6 rounded-2xl p-6 shadow-lg">
             {/* Materi Belajar */}
             <div className="flex justify-between">
               <h1 className="text-xl font-bold">Materi Belajar</h1>
               <div className="flex w-fit items-center justify-between gap-2 rounded-3xl">
-                <TbProgressCheck size={30} color="#22c55e" className="hidden lg:flex md:hidden"/>
+                <TbProgressCheck
+                  size={30}
+                  color="#22c55e"
+                  className="hidden md:hidden lg:flex"
+                />
                 <div className="rounded-3xl bg-primary px-3 py-1 font-bold text-white">
                   50% Completed
                 </div>
@@ -269,16 +284,22 @@ export const DetailKelas = () => {
           <h1 className="font-semibold text-primary">Kelas Premium</h1>
         </DialogHeader>
         <DialogBody className="px-12">
-          <CardKursus
-            category={"UI/UX DESIGN"}
-            rating={"5.0"}
-            title={"Intro to Basic of User Interaction Design"}
-            author={"Simon Doe"}
-            level={"Beginner"}
-            modul={5}
-            duration={4}
-            price={"349.000"}
-          />
+          {storeDetailCourses === null ? (
+            <CardCoursesSkeleton />
+          ) : (
+              <CardKursus
+                image={storeDetailCourses?.courseImg}
+                category={storeDetailCourses?.category?.categoryName}
+                rating={storeDetailCourses?.averageRating}
+                title={storeDetailCourses?.courseName}
+                author={storeDetailCourses?.mentor}
+                level={storeDetailCourses?.level}
+                modul={storeDetailCourses?.modul}
+                duration={storeDetailCourses?.duration}
+                price={storeDetailCourses?.price}
+                isPremium={storeDetailCourses?.isPremium}
+              />
+          )}
         </DialogBody>
         <DialogFooter className="flex justify-center">
           <div className="flex w-64 cursor-pointer items-center justify-center gap-3 rounded-full bg-primary py-2 transition-all hover:bg-primary-hover">
