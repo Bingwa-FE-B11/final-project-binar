ac500c1c87f09fb9bbc63ee19347072caab281dd
Ferry Febriansyah <perriebenhard@gmail.com>
2023-12-31 10:56:44 +0700

    fix: active item and handle user

--- src/assets/components/navbar/NavbarMobile.jsx
+++ src/assets/components/navbar/NavbarMobile.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { Dialog, DialogBody } from "@material-tailwind/react";
 
 // Icons
@@ -13,11 +13,14 @@ import { IoSettingsOutline } from "react-icons/io5";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { LuLogOut } from "react-icons/lu";
 import { logoutUserAction } from "../../../redux/action/auth/logoutUserAction";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 export const NavbarMobile = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const location = useLocation();
+
+  const storeAuthUser = useSelector((state) => state.authLogin);
 
   const [open, setOpen] = React.useState(false);
 
@@ -29,7 +32,7 @@ export const NavbarMobile = () => {
 
   return (
     <>
-      <div className="fixed bottom-0 w-full shadow-2xl z-50">
+      <div className="fixed bottom-0 z-50 w-full shadow-2xl">
         <div className="flex h-full items-center justify-between bg-white px-5 py-2">
           <div
             className="flex flex-col items-center justify-center gap-2"
@@ -37,8 +40,23 @@ export const NavbarMobile = () => {
               navigate("/");
             }}
           >
-            <IoHomeOutline size={25} color="gray" />
-            <span className="text-sm text-slate-500">Home</span>
+            <div
+              className={
+                location.pathname === "/" ? `text-primary` : `text-slate-500`
+              }
+            >
+              <IoHomeOutline size={25} />
+            </div>
+            <span
+              className={`text-sm
+                ${
+                  location.pathname === "/"
+                    ? `font-semibold text-primary`
+                    : `text-slate-500`
+                }`}
+            >
+              Home
+            </span>
           </div>
           <div
             className="flex flex-col items-center justify-center gap-2"
@@ -46,8 +64,25 @@ export const NavbarMobile = () => {
               navigate("/notifikasi");
             }}
           >
-            <IoNotificationsOutline size={25} color="gray" />
-            <span className="text-sm text-slate-500">Notifikasi</span>
+            <div
+              className={
+                location.pathname === "/notifikasi"
+                  ? `text-primary`
+                  : `text-slate-500`
+              }
+            >
+              <IoNotificationsOutline size={25} />
+            </div>
+            <span
+              className={`text-sm
+                ${
+                  location.pathname === "/notifikasi"
+                    ? `font-semibold text-primary`
+                    : `text-slate-500`
+                }`}
+            >
+              Notifikasi
+            </span>
           </div>
           <div
             className="flex flex-col items-center justify-center gap-2"
@@ -55,8 +90,25 @@ export const NavbarMobile = () => {
               navigate("/kelas-saya");
             }}
           >
-            <FaRegCirclePlay size={25} color="gray" />
-            <span className="text-sm text-slate-500">Kelas</span>
+            <div
+              className={
+                location.pathname === "/kelas-saya"
+                  ? `text-primary`
+                  : `text-slate-500`
+              }
+            >
+              <FaRegCirclePlay size={25} />
+            </div>
+            <span
+              className={`text-sm
+                ${
+                  location.pathname === "/kelas-saya"
+                    ? `font-semibold text-primary`
+                    : `text-slate-500`
+                }`}
+            >
+              Kelas
+            </span>
           </div>
           <div
             className="flex flex-col items-center justify-center gap-2"
@@ -64,8 +116,29 @@ export const NavbarMobile = () => {
               navigate("/all-kelas");
             }}
           >
-            <CiCircleList size={25} color="gray" />
-            <span className="text-sm text-slate-500">Kursus</span>
+            <div
+              className={
+                location.pathname === "/all-kelas" ||
+                location.pathname === "/pilih-gratis" ||
+                location.pathname === "/pilih-premium"
+                  ? `text-primary`
+                  : `text-slate-500`
+              }
+            >
+              <CiCircleList size={25} />
+            </div>
+            <span
+              className={`text-sm
+                ${
+                  location.pathname === "/all-kelas" ||
+                  location.pathname === "/pilih-gratis" ||
+                  location.pathname === "/pilih-premium"
+                    ? `font-semibold text-primary`
+                    : `text-slate-500`
+                }`}
+            >
+              Kursus
+            </span>
           </div>
           <div
             className="flex flex-col items-center justify-center gap-2"
@@ -76,53 +149,71 @@ export const NavbarMobile = () => {
           </div>
         </div>
       </div>
+
+      {/* Dialog Navbar Mobile */}
       <Dialog open={open} handler={handleOpen}>
         <DialogBody>
-          <div
-            className="flex cursor-pointer items-center gap-3 border-b-2 border-slate-300 py-4 hover:text-primary"
-            onClick={() => {
-              navigate("/akun-profile");
-            }}
-          >
-            <div className="text-primary">
-              <LuPenLine size={25} />
-            </div>
-            <div className="text-md font-semibold">Profil Saya</div>
-          </div>
+          {storeAuthUser.token ? (
+            <>
+              <div
+                className="flex cursor-pointer items-center gap-3 border-b-2 border-slate-300 py-4 hover:text-primary"
+                onClick={() => {
+                  navigate("/akun-profile");
+                }}
+              >
+                <div className="text-primary">
+                  <LuPenLine size={25} />
+                </div>
+                <div className="text-md font-semibold">Profil Saya</div>
+              </div>
 
-          <div
-            className="flex cursor-pointer items-center gap-3 border-b-2 border-slate-300 py-4 hover:text-primary"
-            onClick={() => {
-              navigate("/akun-password");
-            }}
-          >
-            <div className="text-primary">
-              <IoSettingsOutline size={25} />
-            </div>
-            <div className="text-md font-semibold">Ubah Password</div>
-          </div>
+              <div
+                className="flex cursor-pointer items-center gap-3 border-b-2 border-slate-300 py-4 hover:text-primary"
+                onClick={() => {
+                  navigate("/akun-password");
+                }}
+              >
+                <div className="text-primary">
+                  <IoSettingsOutline size={25} />
+                </div>
+                <div className="text-md font-semibold">Ubah Password</div>
+              </div>
 
-          <div
-            className="flex cursor-pointer items-center gap-3 border-b-2 border-slate-300 py-4 hover:text-primary"
-            onClick={() => {
-              navigate("/akun-pembayaran");
-            }}
-          >
-            <div className="text-primary">
-              <MdOutlineShoppingCart size={25} />
-            </div>
-            <div className="text-md font-semibold">Riwayat Pembayaran</div>
-          </div>
+              <div
+                className="flex cursor-pointer items-center gap-3 border-b-2 border-slate-300 py-4 hover:text-primary"
+                onClick={() => {
+                  navigate("/akun-pembayaran");
+                }}
+              >
+                <div className="text-primary">
+                  <MdOutlineShoppingCart size={25} />
+                </div>
+                <div className="text-md font-semibold">Riwayat Pembayaran</div>
+              </div>
 
-          <div
-            className="flex cursor-pointer items-center gap-3 border-b-2 border-slate-300 py-4 hover:text-primary"
-            onClick={handleLogout}
-          >
-            <div className="text-primary">
-              <LuLogOut size={25} />
+              <div
+                className="flex cursor-pointer items-center gap-3 py-4 hover:text-primary"
+                onClick={handleLogout}
+              >
+                <div className="text-primary">
+                  <LuLogOut size={25} />
+                </div>
+                <div className="text-md font-semibold">Keluar</div>
+              </div>
+            </>
+          ) : (
+            <div
+              className="flex cursor-pointer items-center gap-3 py-4 hover:text-primary"
+              onClick={() => {
+                navigate("/login");
+              }}
+            >
+              <div className="text-primary">
+                <LuLogOut size={25} />
+              </div>
+              <div className="text-md font-semibold">Masuk</div>
             </div>
-            <div className="text-md font-semibold">Keluar</div>
-          </div>
+          )}
         </DialogBody>
       </Dialog>
     </>
