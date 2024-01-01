2f93aadf69e72388952746f4250c41e90e843745
Ferry Febriansyah <perriebenhard@gmail.com>
2024-01-01 11:21:40 +0700

    fix: remove sidebar in PilihKelas Page

--- src/pages/user/kelas/PilihKelas.jsx
+++ src/pages/user/kelas/PilihKelas.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -9,7 +9,6 @@ import { BiSearchAlt } from "react-icons/bi";
 import { NavbarKelas } from "../../../assets/components/navbar/NavbarKelas";
 import { CardGlobal } from "../../../assets/components/cards/CardGlobal";
 import { NavbarHome } from "../../../assets/components/navbar/NavbarHome";
-import { SidebarKelas } from "../../../assets/components/sidebar/SidebarKelas";
 import LoadingSpinner from "../../../assets/components/loading/loadingSpinner";
 import { searchCourseAction } from "../../../redux/action/courses/searchCourseAction";
 import { CardPremium } from "../../../assets/components/cards/CardPremium";
@@ -32,18 +31,6 @@ export const PilihKelas = () => {
     (state) => state.dataCourses.searchedCourses,
   );
   const isLoading = useSelector((state) => state.dataCourses.loading);
-  const storeFilteredCourses = useSelector(
-    (state) => state.dataCourses.filteredCourses,
-  );
-
-  // Filter Feature
-  const [displayedCourses, setDisplayedCourses] = useState([]);
-
-  useEffect(() => {
-    const coursesToDisplay =
-      storeSearchedCourses?.length > 0 ? storeSearchedCourses : [];
-    setDisplayedCourses(coursesToDisplay);
-  }, [storeFilteredCourses, storeSearchedCourses]);
 
   if (isLoading) {
     return <LoadingSpinner />;
@@ -54,6 +41,7 @@ export const PilihKelas = () => {
     const search = dispatch(searchCourseAction(searchInput));
 
     if (search) {
+      setSearchInput("");
       navigate(`/pilih-kelas?search=${searchInput}`);
     }
   };
@@ -67,9 +55,9 @@ export const PilihKelas = () => {
       ) : (
         <NavbarKelas />
       )}
-      <div className="flex h-fit flex-col justify-between bg-secondary md:h-screen lg:h-fit">
+      <div className="flex h-fit min-h-screen flex-col justify-between bg-secondary">
         {isMobile ? <SearchMobile /> : <></>}
-        <div className="flex flex-col justify-center px-4 pb-16 pt-4 md:px-4 md:pb-0 md:pt-20 lg:px-24 lg:pb-0 lg:pt-20">
+        <div className="flex flex-col justify-center px-4 pb-16 pt-4 md:px-4 md:pt-20 lg:px-24 lg:pt-20">
           {/* Search */}
           <div className="flex items-center justify-between py-4">
             <div className="-mt-8 py-6 text-xl font-bold md:mt-0 md:px-4 md:text-3xl lg:mt-0 lg:px-0 lg:text-3xl">
@@ -100,58 +88,22 @@ export const PilihKelas = () => {
             )}
           </div>
 
-          <div className="flex items-start justify-center py-4 md:justify-between lg:justify-between">
-            {/* Filter */}
-            <div className="hidden w-[30%] md:flex lg:flex">
-              <SidebarKelas />
-            </div>
-
+          <div className="flex items-start justify-center py-4 md:justify-between md:py-0 lg:justify-between lg:py-0">
             {/* Button */}
-            <div className="flex w-full flex-wrap items-center justify-between font-semibold md:w-[65%] lg:w-[65%]">
-              {isMobile ? (
-                <></>
-              ) : (
-                <div className="flex w-full gap-4 text-center">
-                  <div
-                    className="w-[20%] cursor-pointer rounded-xl bg-white py-2 hover:bg-primary hover:text-white"
-                    onClick={() => {
-                      navigate("/all-kelas");
-                    }}
-                  >
-                    <button>All</button>
-                  </div>
-                  <div
-                    className="w-[40%] cursor-pointer rounded-xl bg-white py-2 hover:bg-primary hover:text-white md:w-[50%] lg:w-[60%]"
-                    onClick={() => {
-                      navigate("/pilih-premium");
-                    }}
-                  >
-                    <button>Kelas Premium</button>
-                  </div>
-                  <div
-                    className="w-[30%] cursor-pointer rounded-xl bg-white py-2 hover:bg-primary hover:text-white md:w-[40%] lg:w-[30%]"
-                    onClick={() => {
-                      navigate("/pilih-gratis");
-                    }}
-                  >
-                    <button>Kelas Gratis</button>
-                  </div>
-                </div>
-              )}
-
-              <div className="-mt-12 py-4 md:mt-0 lg:mt-0">
+            <div className="flex w-full flex-wrap items-center justify-between px-0 md:px-5 lg:px-0">
+              <div className="-mt-12 font-medium md:mt-0 md:py-4 md:text-lg lg:mt-0 lg:py-4 lg:text-lg">
                 Menampilkan{" "}
                 <span className="font-bold text-primary">"{searchParam}"</span>
               </div>
               {/* Main Content */}
-              <div className="grid w-full grid-cols-1 gap-6 py-2 md:grid-cols-1 lg:grid-cols-2">
+              <div className="grid w-full grid-cols-1 gap-6 py-2 md:grid-cols-2 lg:grid-cols-3">
                 {/* Card Item */}
-                {displayedCourses.length === 0 ? (
-                  <p className="col-span-2 py-10 text-center text-lg font-semibold italic text-slate-500">
+                {storeSearchedCourses.length === 0 ? (
+                  <p className="col-span-3 py-10 text-center text-lg font-semibold italic text-slate-500">
                     - Course tidak ditemukan -
                   </p>
                 ) : (
-                  displayedCourses.map((value) =>
+                  storeSearchedCourses.map((value) =>
                     value.isPremium ? (
                       <CardPremium
                         key={value.id}
@@ -187,7 +139,6 @@ export const PilihKelas = () => {
             </div>
           </div>
         </div>
-        {isMobile ? <NavbarMobile /> : <NavbarKelas style={{ zIndex: 1 }} />}
       </div>
     </>
   );
